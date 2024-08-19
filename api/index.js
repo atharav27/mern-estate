26194c3cc273d2037c03c79b75c969911da9d349
atharav27 <110390023+atharav27@users.noreply.github.com>
2024-08-20 00:11:02 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import  mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config();
+// console.log(process.env.MONGO); 
+mongoose
+  .connect("mongodb+srv://atharavbhawsar06:BHV13yLMlpodRJPE@mern-estatehub.85yns.mongodb.net/?retryWrites=true&w=majority&appName=mern-estatehub")
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((error) => {
+    console.error("Connection failed!", error);
+  });
+
 
 const app = express();
 
 app.listen(3000, ()=>{
     console.log('server is running on port 3000')
-})
+}) 
